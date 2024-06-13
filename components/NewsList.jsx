a52a0017e217ccda38425e1d0fa5f705764c3c83
Adiksuu <codeadiksuu@gmail.com>
2024-06-13 17:37:48 +0200

    Removed unecessary text

--- components/NewsList.jsx
+++ components/NewsList.jsx
@@ -5,7 +5,7 @@ export default function NewsList({ darkmode }) {
   const notes = [
     {
       header: 'Update v1.1.0',
-      title: 'The application will soon leave the test phase (BETA), I am currently working on a new version of the application (v1.1.0), for which you will be able to receive a pre-release version within a few days. Below is an (incomplete) record of everything changed in v1.1.0',
+      title: 'The application will soon leave the test phase (BETA), I am currently working on a new version of the application (v1.1.0), for which you will be able to receive a pre-release version within a few days.',
       date: '13.06.2024',
       category: 'New update',
       icon: 'chart-line'
