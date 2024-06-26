b5ca1cfd1514dfc076fa0f0af80daf4ffe09b191
Adiksuu <codeadiksuu@gmail.com>
2024-06-26 10:59:57 +0200

    Changed the Loading View

--- screens/Loading.js
+++ screens/Loading.js
@@ -1,8 +1,38 @@
-import { View } from 'react-native'
+import { Animated, Easing, StyleSheet, View } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { black } from '../utility/colors';
 
 export default function Loading() {
+  const spinValue = new Animated.Value(0);
+
+  Animated.loop(
+    Animated.timing(spinValue, {
+      toValue: 1,
+      duration: 2000,
+      easing: Easing.linear,
+      useNativeDriver: true
+    })
+  ).start();
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  });
+
   return (
-    <View></View>
+    <View style={styles.content}>
+      <Animated.View style={{ transform: [{ rotate: spin }] }}>
+        <MaterialCommunityIcons name="loading" size={80} color={black} />
+      </Animated.View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
